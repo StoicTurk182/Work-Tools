005b54b919d6a8277c58d2abcbdb8ab552da0ac8
Andrew J Jones <154071865+StoicTurk182@users.noreply.github.com>
2024-07-30 15:04:01 +0100

    Update WUALL.ps1

--- Update_Scripts/WUALL.ps1
+++ Update_Scripts/WUALL.ps1
@@ -1,7 +1,11 @@
-Set-ExecutionPolicy bypass -Scope process
-Install-Module -Name PSWindowsUpdate -AllowClobber -Force
+# Allow script execution temporarily for this session
+Set-ExecutionPolicy Bypass -Scope Process -Force
 
+# Install the PSWindowsUpdate module, allowing it to overwrite any existing module
+Install-Module -Name PSWindowsUpdate -AllowClobber -Force -Scope CurrentUser
+
+# Import the PSWindowsUpdate module into the current session
 Import-Module PSWindowsUpdate
 
-# Get updates (including optional)
+# Get and install Windows updates (including optional updates), accepting all updates and ignoring reboot prompts
 Get-WindowsUpdate -MicrosoftUpdate -AcceptAll -Install -IgnoreReboot
