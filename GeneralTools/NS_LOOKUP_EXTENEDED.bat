9b74d25ee094904668597598a861e6310495f1ef
Andrew J Jones <154071865+StoicTurk182@users.noreply.github.com>
2024-08-02 16:49:46 +0100

    Add files via upload

---
+++ GeneralTools/NS_LOOKUP_EXTENEDED.bat
@@ -0,0 +1,24 @@
+@echo off
+:: Check for administrative rights
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+:loop
+:: Prompt for hostname and run nslookup
+set /p hostname=Enter hostname: 
+if "%hostname%"=="" goto :eof
+nslookup %hostname%
+
+:: Prompt for additional actions
+set /p action=Do you want to ping this hostname? (y/n): 
+if /i "%action%"=="y" ping %hostname%
+
+set /p action=Do you want to run ipconfig? (y/n): 
+if /i "%action%"=="y" ipconfig
+
+echo.
+goto loop
