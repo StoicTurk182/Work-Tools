47f9b9727fd979f37550cf7614a407be8156661e
Andrew J Jones <154071865+StoicTurk182@users.noreply.github.com>
2024-08-23 09:31:22 +0100

    Delete Bat_Credential directory

--- Bat_Credential/Read_xml.ps1
+++
@@ -1,8 +0,0 @@
-# Define the path for the XML file
-$xmlFilePath = "C:\Users\Orion\Desktop\Bat_Credential\encrypted_credentials.xml"
-
-# Import the XML file to get the credentials object
-$credential = Import-CliXml -Path $xmlFilePath
-
-# Use the credentials (example)
-Write-Host "Username: $($credential.UserName)"
